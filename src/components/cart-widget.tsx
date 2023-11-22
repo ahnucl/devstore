b77d128ebbfa0e30489ea26e9ec6346479ce1e76
Leonardo Cunha <leonardo.cunha@comunix.net.br>
2023-11-22 09:45:09 -0300

    usa contexto do carrinho

---
+++ src/components/cart-widget.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+import { useCart } from '@/contexts/cart-contexts'
+import { ShoppingBag } from 'lucide-react'
+
+export function CartWidget() {
+  const { items } = useCart()
+
+  const quantityOfItems = items.reduce((acc, item) => acc + item.quantity, 0)
+
+  return (
+    <div className="flex items-center gap-2">
+      <ShoppingBag className="w-4 h-4" />
+      <span className="text-sm">Carrinho ({quantityOfItems})</span>
+    </div>
+  )
+}
